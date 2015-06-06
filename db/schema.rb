f006465c3799639f517492a4ec071d3a835b98fe
ysc6688 <ysc6688@ustc.edu.cn>
2015-06-06 23:40:43 +0800

    user role

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20150606131557) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -28,9 +27,9 @@ ActiveRecord::Schema.define(version: 20150606131557) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "role"
+    t.string   "email"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
